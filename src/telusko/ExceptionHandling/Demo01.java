3e1afbfcba438984522b5eac10f5f72c65c37bd1
mykhaylo-zhovkevych <mzovkevic@gmail.com>
2024-04-14 18:05:17 +0200

    commit

--- src/telusko/ExceptionHandling/Demo01.java
+++ src/telusko/ExceptionHandling/Demo01.java
@@ -1,5 +1,7 @@
 package telusko.ExceptionHandling;
 
+import java.io.*;
+
 // the own exception can be created, but it needs a tool, whether I build the exception(runtime) class or inherit from it is up to me
 class MyException extends Exception {
 
@@ -29,5 +31,17 @@ public class Demo01 {
          catch(Exception e) {
             System.out.println(e);
          }
+    }
+}
+
+/* The throws keyword is used in the method signature to declare that a method might throw certain exceptions during its execution. Declaring an exception with throws does not throw the 
+exception itself—it only signals that the method could throw this exception, leaving it up to the caller of the method to handle it. This is primarily used with checked exceptions, 
+which must be either caught or declared to be thrown. Here’s how you use it:
+ */
+class FileReader {
+    public void readFile(String path) throws IOException {
+        File file = new File(path);
+        FileInputStream stream = new FileInputStream(file);
+        // Further file processing code
     }
 }
